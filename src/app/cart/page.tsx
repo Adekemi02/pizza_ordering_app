b7d5a15d3633ed24908d3d6c1bed9ccd3d531e70
Adisa Barakat <adisabarakatadekemi@gmail.com>
2023-12-18 16:20:49 +0100

    Modifies the cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import React, { useEffect } from 'react'
 import { toast } from 'react-toastify';
 
+
+
 const CartPage = () => {
 
     const { totalPrice, products, removeFromCart } = usePizzaStore();
@@ -18,47 +20,46 @@ const CartPage = () => {
     <div className='w-full px-4 flex justify-start  items-start mt-11'>
       {/* PRODUCT TABLE */}
       <div className="w-full">
-        {
-          
-            <table className="w-full">
-          <thead className="border-b border-b-gray-200">
-            <tr className="text-left">
-              <th className="mb-2 py-2">Product</th>
-              <th className="mb-2 py-2">Name</th>
-              <th className="mb-2 py-2">Extra</th>
-              <th className="mb-2 py-2">Price</th>
-              <th className="mb-2 py-2">Quantity</th>
-              <th className="mb-2 py-2">Total</th>
-            </tr>
-          </thead>
-          
-          {
-            products.map((product)=> (
-              <tbody className="border-b border-b-gray-200">
+        {         
+          <table className="w-full">
+            <thead className="border-b border-b-gray-200">
+              <tr className="text-left">
+                <th className="mb-2 py-2">Product</th>
+                <th className="mb-2 py-2">Name</th>
+                <th className="mb-2 py-2">Extra</th>
+                <th className="mb-2 py-2">Price</th>
+                <th className="mb-2 py-2">Quantity</th>
+                <th className="mb-2 py-2">Total</th>
+              </tr>
+            </thead>
             
-            <tr className="align-top" >
-              <td className="py-2">
-                <div className="flex flex-row justify-center items-center">
-                  <div className="">
-                  {product.img && <Image src={product.img} alt="" width={70} height={70} /> }
-                  </div>
-                  <div className="bg-white" onClick={()=> {removeFromCart(product), toast("item removed with love from olaide!")}}>
-                  <Image src="/delete.png" alt="" width={20} height={20} />
-                  </div>
-                </div>
-                
-              </td>
-              <td className=" py-2"> {product.title} </td>
-              <td className=" py-2">{product.options}</td>
-              <td className=" py-2">${product.itemPrice}</td>
-              <td className=" py-2">{product.quantity}</td>
-              <td className=" py-2">${product.price}</td>
-            </tr>
-          </tbody> 
-            ))
-          }                
-        </table>
-          
+            {products.map((product)=> (
+              <tbody key={product.id} className="border-b border-b-gray-200">
+            
+                <tr className="align-top" >
+                  <td className="py-2">
+
+                    <div className="flex flex-row justify-center items-center">                      
+                      <div className="">
+                        {product.img && <Image src={product.img} alt="" width={70} height={70} /> }
+                      </div>
+                      
+                      <div className="bg-white" onClick={()=> {removeFromCart(product), toast("item removed with love from olaide!")}}>
+                        <Image src="/delete.png" alt="" width={20} height={20} />
+                      </div>
+                    </div>      
+                  </td>
+                  
+                  <td className=" py-2"> {product.title} </td>
+                  <td className=" py-2">{product.options}</td>
+                  <td className=" py-2">${product.itemPrice}</td>
+                  <td className=" py-2">{product.quantity}</td>
+                  <td className=" py-2">${product.price}</td>
+                </tr>
+              </tbody> 
+            )
+            )}                
+          </table>          
         }
       </div>
 
