98a76ff2cbc69dec44067fdc6e275c1e1a958df1
RedOx98 <olaskeet@gmail.com>
2023-12-18 14:24:46 +0100

    with love from olaide

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
+import { usePizzaStore } from '@/utils/store';
 
 const CartIcon = () => {
+    const { totalItems } = usePizzaStore();
+  
+    useEffect(()=> {
+      usePizzaStore.persist.rehydrate();
+    },[]);
   return (
     <Link href="/cart">
         <div className='relative'>
             <Image src="/images/cart_logo_dark.png" alt="Cart Image" width={25} height={25} />
-            <span className="absolute top-[-5px] right-[-8px] bg-red-600 text-white text-center w-[15px] h-[15px] flex justify-center items-center rounded-3xl"> 0 </span>
+            <span className="absolute top-[-5px] right-[-8px] bg-red-600 text-white text-center w-[15px] h-[15px] flex justify-center items-center rounded-3xl"> {totalItems} </span>
         </div>
     </Link>
   )
---
+++ src/types/types.ts
@@ -0,0 +1,29 @@
+export type Product = {
+    id: number;
+    title: string;
+    desc?: string;
+    img?: string;
+    price: number;
+    options?: { title: string; additionalPrice: number; width: number; height: number }[];
+  };
+
+export type CartItemType = {
+    id: number,
+    title: string;
+    desc?: string;
+    img?: string;
+    price: number;
+    options?: string;
+    quantity: number;
+};
+
+export type CartType = {
+    products: CartItemType[];
+    totalItems: number;
+    totalPrice: number;
+};
+
+export type ActionTypes = {
+    addToCart: (item: CartItemType) => void;
+    removeFromCart: (item: CartItemType) => void;
+};
