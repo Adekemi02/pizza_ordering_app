fc953c5b5b20de58df9c87eb5980ac4e4d6b6139
Adisa Barakat <adisabarakatadekemi@gmail.com>
2024-01-14 23:14:50 +0100

    modifies code

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,34 +1,9 @@
-import Products from '@/components/Products'
-import Image from 'next/image'
 import React from 'react'
 
-const productPage = () => {
+const menuPage = () => {
   return (
-    <div className="mt-10"> 
-      <div className="flex justify-between items-center px-4">
-        <button className="bg-goldenyellow text-white flex justify-center items-center m-2 w-24 rounded-full">
-          <Image src="/images/filter_menu.png" alt="" width={25} height={25} />
-          <span className="text-center font-semibold text-sm my-2"> Filter </span>
-        </button>
-        <div className="flex gap-10">
-          <div className="font-bold">Showing all 9 results </div>
-          <div className="w-[250px] h-9 border flex justify-between">
-            
-            <span className="px-2 mt-1 text-center"> Default Sorting </span>
-            <div className="mt-2 px-2">
-              <Image src="/images/angle.png" alt="" width={15} height={15} />
-            </div>           
-          </div>
-        </div>
-      </div>
-
-      <div className="">
-        <Products />
-      </div>
-            
-      
-    </div>
+    <div>Menu Page</div>
   )
 }
 
-export default productPage
+export default menuPage
--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -3,29 +3,19 @@
 import Link from 'next/link'
 import React, { useEffect } from 'react'
 import Image from 'next/image'
-import {usePizzaStore} from '@/utils/store';
-import { useSelector } from "react-redux";
-import { RootState } from '@/store/store';
+import { usePizzaStore } from '@/utils/store';
 
-
-
-const CartIcon: React.FC = () => {
-  const quantity = useSelector((state: RootState) => state.cartReducer.items);
-
-  const { totalItems } = usePizzaStore();
-
-  useEffect(()=> {
-    usePizzaStore.persist.rehydrate();
-  },  []);
-
-
+const CartIcon = () => {
+    const { totalItems } = usePizzaStore();
+  
+    useEffect(()=> {
+      usePizzaStore.persist.rehydrate();
+    },[]);
   return (
-    <Link href="/cart" passHref>
+    <Link href="/cart">
         <div className='relative'>
             <Image src="/images/cart_logo_dark.png" alt="Cart Image" width={25} height={25} />
-            <span className="absolute top-[-5px] right-[-8px] bg-red-600 text-white text-center w-[15px] h-[15px] flex justify-center items-center rounded-3xl"> 
-              {quantity.length} 
-            </span>
+            <span className="absolute top-[-5px] right-[-8px] bg-red-600 text-white text-center w-[15px] h-[15px] flex justify-center items-center rounded-3xl"> {totalItems} </span>
         </div>
     </Link>
   )
